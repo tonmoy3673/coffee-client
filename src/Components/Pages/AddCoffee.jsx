e8f6f550cba8e2e7dc52bb34bf7c9c47a94111f7
tonmoy <tonmoy3673@gmail.com>
2024-11-09 00:07:34 +0600

    :tada: implemented add product button

--- src/Components/Pages/AddCoffee.jsx
+++ src/Components/Pages/AddCoffee.jsx
@@ -1,6 +1,27 @@
 
 
 const AddCoffee = () => {
+
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const category = form.category.value;
+
+        const coffeeItems = {
+            name,chef,supplier,taste,details,photo,category
+        }
+        console.log(coffeeItems);
+        window.alert('Coffee Added Successfully!!')
+        form.reset();
+    }
+
+  
     return (
         <div className="bg-slate-200 py-6 lg:py-12 px-4">
            <h2 className="text-xl lg:text-2xl font-semibold text-center py-2 lg:py-6">Add New Coffee</h2>
@@ -8,18 +29,18 @@ const AddCoffee = () => {
             <p className="mb-2 md:mb-4">Discover our latest coffee additions, crafted to elevate your experience with unique flavors and aromas. Explore freshly curated blends from around the world, each with its distinct character. Add a new coffee to your collection and enjoy a taste journey like never before!</p>
            </div>
            <div>
-            <form className="flex items-center justify-center">
+            <form onSubmit={handleSubmit} className="flex items-center justify-center">
                <div>
                  {/* ======= 1st row =========== */}
                  <div className="block md:flex items-center justify-center gap-2 lg:gap-24 ">
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="name" className="block">Name</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="name"  placeholder="Enter coffee name"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="name"  placeholder="Enter coffee name"/>
                     </div>
                     
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="chef" className="block">Chef</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="name"  placeholder="Enter Chef name"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="chef"  placeholder="Enter Chef name"/>
                     </div>
                     
                 </div>
@@ -28,12 +49,12 @@ const AddCoffee = () => {
                 <div className="block md:flex items-center justify-center gap-2 lg:gap-24 mt-3 ">
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="supplier" className="block">Supplier</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="supplier"  placeholder="Enter Coffee Supplier"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="supplier"  placeholder="Enter Coffee Supplier"/>
                     </div>
                     
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="taste" className="block">Taste</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="taste"  placeholder="Enter Coffee Taste"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="taste"  placeholder="Enter Coffee Taste"/>
                     </div>
                     
                 </div>
@@ -42,12 +63,12 @@ const AddCoffee = () => {
                 <div className="block md:flex items-center justify-center gap-2 lg:gap-24 mt-3 ">
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="category" className="block">Category</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="category"  placeholder="Enter Coffee Category"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="category"  placeholder="Enter Coffee Category"/>
                     </div>
                     
                     <div className="text-center md:text-start mt-3">
                     <label htmlFor="details" className="block">Details</label>
-                    <input className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="details"  placeholder="Enter Coffee details"/>
+                    <input required className="w-full lg:w-72 px-3 py-1 mt-2 rounded-md" type="text" id="details"  placeholder="Enter Coffee details"/>
                     </div>
                     
                 </div>
@@ -55,13 +76,13 @@ const AddCoffee = () => {
                 {/* ============ single row ========== */}
                 <div className="text-center md:text-start mt-3">
                     <label htmlFor="photo" className="block">Photo</label>
-                    <input className="w-full lg:w-[672px] px-3 py-1 mt-2 rounded-md" type="text" id="photo"  placeholder="Enter Coffee Photo"/>
+                    <input required className="w-full lg:w-[672px] px-3 py-1 mt-2 rounded-md" type="text" id="photo"  placeholder="Enter Coffee Photo"/>
                     </div>
 
                     {/* ================ Submit button =============== */}
                     <div className="py-2 lg:py-4 mt-2">
-                        <button className="bg-[#967259] text-white w-full lg:w-[672px] rounded-md py-1 hover:bg-amber-500 border-2 border-slate-950">
-                            Submit
+                        <button type="submit"  id="submit" className="bg-[#967259] text-white w-full lg:w-[672px] rounded-md py-1 hover:bg-amber-500 border-2 border-slate-950">
+                           Submit
                         </button>
 
                     </div>
