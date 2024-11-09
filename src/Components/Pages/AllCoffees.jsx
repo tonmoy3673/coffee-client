997e4cf7bd1fc27ae6f4e39b6bb08cd1938691e2
tonmoy <tonmoy3673@gmail.com>
2024-11-09 21:52:50 +0600

    :tada: display coffees

--- src/Components/Pages/AllCoffees.jsx
+++ src/Components/Pages/AllCoffees.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Coffee from "./Coffee";
 
 
 const AllCoffees = () => {
@@ -8,6 +9,11 @@ const AllCoffees = () => {
         <div className="py-8 lg:py-16 ">
             <h2 className="text-xl md:text-3xl font-semibold text-center py-4">
                 Total Coffees : {coffees.length} </h2>
+                <div className="py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-6 md:gap-10">
+                    {
+                       coffees && coffees.map((coffee)=><Coffee key={coffee._id} coffee={coffee}/>) 
+                    }
+                </div>
         </div>
     );
 };
--- src/Components/Pages/Coffee.jsx
+++ src/Components/Pages/Coffee.jsx
@@ -1,9 +1,17 @@
 
 
-const Coffee = () => {
+const Coffee = ({coffee}) => {
+    const {name,chef,_id,photo,price}= coffee;
     return (
-        <div>
-            <h1>This is Coffee Page</h1>
+        <div className="flex items-center bg-slate-200 rounded-md">
+            <div>
+                <img src={photo}/>
+            </div>
+            <div>
+                <h3><span className="font-semibold">Name</span> : {name}</h3>
+                <h4><span className="font-semibold">Chef</span> : {chef}</h4>
+                <p className="text-red-500"> <span className="font-semibold text-black">Price</span> : {price} $</p>
+            </div>
         </div>
     );
 };
